115731fee8c667b68ca4f60a61de74384be7f38a
Wyess <diyhacker@mail.goo.ne.jp>
2022-01-15 20:41:08 +0900

    Initial commit

---
+++ speech_speed_changer_gui.py
@@ -0,0 +1,305 @@
+#!/usr/bin/env python3
+
+from enum import Enum
+import os
+import shutil
+import subprocess
+import wx
+
+class AudioItem:
+    def __init__(self, in_file='', speed_list=None, out_directory='', outFormat=None):
+        self.input = in_file 
+        self.speed_list = speed_list if speed_list else []
+        self.out_directory = out_directory
+        self.output = []
+        self.outFormat = outFormat
+
+        head, tail = os.path.split(in_file)
+        base, _ = os.path.splitext(tail)
+        ext = self.outFormat['ext']
+
+        self.output = [os.path.join(self.out_directory, f"{base}_x{speed}{ext}") for speed in speed_list]
+        self.out_files = [f"{base}_x{speed}{ext}" for speed in speed_list]
+        self.commands = []
+        for out in self.output:
+            self.commands.append([])
+            self.commands[-1].append(self.outFormat['cmd'].split(' ')[0])
+            self.commands[-1].insert(1, '-i')
+            self.commands[-1].insert(2, self.input)
+            self.commands[-1].extend(self.outFormat['cmd'].split(' ')[1:])
+            self.commands[-1].append(out)
+
+    def __str__(self):
+        s = self.input
+        s += "\n"
+        s += self.out_directory
+        s += "\n"
+        s += ",".join([str(i) for i in self.speed_list])
+        s += "\n"
+        s += "\n".join(self.output)
+        s += "\n"
+        s += str(self.outFormat)
+        s += "\n"
+        s += str(self.commands)
+        return s
+
+class InputFileDropTarget(wx.FileDropTarget):
+    def __init__(self, callback):
+        wx.FileDropTarget.__init__(self)
+        self.callback = callback
+
+    def OnDropFiles(self, x, y, filenames):
+        self.callback(None, filenames)
+        return True
+
+class State(Enum):
+    IDLE = 0
+    RUNNING = 1
+    INTERRUPTED = 2
+
+class SpeechSpeedChangerGui(wx.Frame):
+    def __init__(self):
+        wx.Frame.__init__(self, parent=None, title='', size=(400, 600))
+        self.SetMinSize((400, 600))
+        self.InitUi()
+        self.in_list = []
+        self.out_dir = ''
+        self.audio_items = []
+        self.processed_files = {} 
+        self.state = State.IDLE
+        self.Show()
+    
+    def InitUi(self):
+        panel = wx.Panel(self, wx.ID_ANY, style=wx.RAISED_BORDER)
+        self.inputTextCtrl = wx.TextCtrl(panel, wx.ID_ANY, size=(-1, 100), style=wx.TE_MULTILINE)
+        self.mergeCheck = wx.CheckBox(panel, wx.ID_ANY, label="Merge")
+
+        self.outDirPicker = wx.DirPickerCtrl(panel, size=(400, -1))
+
+        self.outFormatComboBox = wx.ComboBox(panel, wx.ID_ANY, style=wx.CB_READONLY)
+
+        self.progressGauge = wx.Gauge(panel, wx.ID_ANY, style=wx.GA_HORIZONTAL|wx.GA_PROGRESS)
+        
+        self.presetComboBox = wx.ComboBox(panel, wx.ID_ANY, style=wx.CB_READONLY)
+        self.text = wx.TextCtrl(panel, wx.ID_ANY, style=wx.TE_MULTILINE, size=(-1, 100))
+        self.startButton = wx.Button(panel, wx.ID_ANY, label="Start")
+
+        dt = InputFileDropTarget(self.GenerateParams)
+        self.inputTextCtrl.SetDropTarget(dt)
+
+        self.LoadPresets()
+        self.LoadOutFormat()
+
+        self.mergeCheck.SetValue(True)
+
+        layout = wx.BoxSizer(wx.VERTICAL)
+
+        inputTextCtrlLayout = wx.BoxSizer(wx.HORIZONTAL)
+        listLayout = wx.StaticBoxSizer(wx.VERTICAL, panel, label="Input files")
+        ioLayout = wx.StaticBoxSizer(wx.HORIZONTAL, panel, label="Output")
+        listLayout.Add(self.inputTextCtrl, flag=wx.EXPAND|wx.LEFT|wx.RIGHT, border=5, proportion=1)
+        ioLayout.Add(self.text, flag=wx.EXPAND|wx.LEFT|wx.RIGHT, proportion=1, border=5)
+        layout.Add(listLayout, flag=wx.EXPAND|wx.ALL, border=5, proportion=1)
+        layout.Add(ioLayout, flag=wx.EXPAND|wx.ALL, proportion=1, border=5)
+
+        controlLayout = wx.BoxSizer(wx.HORIZONTAL)
+
+        outDirLayout = wx.StaticBoxSizer(wx.VERTICAL, panel, 'Output directory')
+        outDirLayout.Add(self.outDirPicker)
+        layout.Add(outDirLayout, flag=wx.EXPAND|wx.LEFT|wx.RIGHT, border=5)
+
+        presetLayout = wx.StaticBoxSizer(wx.HORIZONTAL, panel, 'Settings')
+        presetLayout.Add(self.outFormatComboBox, flag=wx.EXPAND)
+        presetLayout.Add(self.presetComboBox, flag=wx.EXPAND|wx.LEFT|wx.RIGHT)
+        presetLayout.Add(self.mergeCheck, flag=wx.EXPAND|wx.LEFT|wx.RIGHT)
+        layout.Add(presetLayout, flag=wx.EXPAND|wx.ALL, border=5)
+
+        checkBoxLayout2 = wx.BoxSizer(wx.HORIZONTAL)
+        layout.Add(checkBoxLayout2, flag=wx.LEFT, border=5)
+        layout.Add(self.startButton, flag=wx.EXPAND|wx.ALL, border=10)
+
+        layout.Add(self.progressGauge, flag=wx.EXPAND|wx.LEFT|wx.RIGHT|wx.BOTTOM, border=15)
+
+        self.Bind(wx.EVT_LISTBOX, self.GenerateParams, self.inputTextCtrl)
+        self.Bind(wx.EVT_BUTTON, self.Convert, self.startButton)
+        self.Bind(wx.EVT_COMBOBOX, self.GenerateParams, self.presetComboBox)
+        self.Bind(wx.EVT_COMBOBOX, self.GenerateParams, self.outFormatComboBox)
+
+        panel.SetSizer(layout)
+
+    def LoadPresets(self):
+        self.presetComboBox.Append('2,3,4', {'speed':[2,3,4]})
+        self.presetComboBox.Append('2,3,4,2', {'speed':[2,3,4,2]})
+        self.presetComboBox.Append('1,2,3,4,1', {'speed':[1,2,3,4,1]})
+        self.presetComboBox.Append('2,3,4,5', {'speed':[2,3,4,5]})
+        self.presetComboBox.Append('2,3,4,5,2', {'speed':[2,3,4,5,2]})
+        self.presetComboBox.Append('1,2,3,4,5,1', {'speed':[2,3,4,5,1]})
+        self.presetComboBox.Append('2,3,4,5,6', {'speed':[2,3,4,5,6]})
+        self.presetComboBox.Append('2,3,4,5,6,2', {'speed':[2,3,4,5,6,2]})
+        self.presetComboBox.Append('1,2,3,4,5,6,1', {'speed':[1,2,3,4,5,6,1]})
+        self.presetComboBox.Append('0.25', {'speed':[0.25]})
+        self.presetComboBox.Append('0.5', {'speed':[0.5]})
+        self.presetComboBox.Append('2', {'speed':[2]})
+        self.presetComboBox.Append('3', {'speed':[3]})
+        self.presetComboBox.Append('4', {'speed':[4]})
+        self.presetComboBox.Append('5', {'speed':[5]})
+        self.presetComboBox.Append('6', {'speed':[6]})
+        self.presetComboBox.SetSelection(0)
+
+    def LoadOutFormat(self):
+        self.outFormatComboBox.Append('ALAC', {'cmd': 'ffmpeg -y -acodec alac', 'ext': '.m4a'})
+        self.outFormatComboBox.Append('FLAC', {'cmd': 'ffmpeg -y -acodec flac', 'ext': '.flac'})
+        self.outFormatComboBox.Append('AIFF', {'cmd': 'ffmpeg -y -acodec pcm_s16be', 'ext': '.aiff'})
+        self.outFormatComboBox.Append('WAV', {'cmd': 'ffmpeg -y -acodec pcm_s16le', 'ext': '.wav'})
+        self.outFormatComboBox.Append('MP3 (192kbps)', {'cmd': 'ffmpeg -y -acodec libmp3lame -ab 192k', 'ext': '.mp3'})
+        self.outFormatComboBox.Append('MP3 (160kbps)', {'cmd': 'ffmpeg -y -acodec libmp3lame mp3 -ab 160k', 'ext': '.mp3'})
+        self.outFormatComboBox.Append('MP3 (128kbps)', {'cmd': 'ffmpeg -y -acodec libmp3lame mp3 -ab 128k', 'ext': '.mp3'})
+        self.outFormatComboBox.Append('AAC (256kbps)', {'cmd': 'ffmpeg -y -acodec aac -ab 128k -ar 44100', 'ext': '.m4a'})
+        self.outFormatComboBox.Append('AAC (128kbps)', {'cmd': 'ffmpeg -y -acodec aac -ab 256k -ar 44100', 'ext': '.m4a'})
+        self.outFormatComboBox.SetSelection(0)
+
+    def RunProcess(self, args):
+        ps = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        while True:
+            line = ps.stdout.readline().decode()
+            if line:
+                self.text.write(line)
+                wx.Yield()
+            else:
+                break
+
+    def Speedup(self, event, audio_item):
+        in_file = audio_item.input
+
+        if in_file.endswith('.wav'):
+            shutil.copy(in_file, 'tmp.wav')
+        else:
+            args = ['ffmpeg', '-i', in_file, 'tmp.wav', '-y']
+            self.RunProcess(args)
+
+        for i, speed in enumerate(audio_item.speed_list):
+            args = ['./sonic', '-q', '-s', f"{speed:.2f}", 'tmp.wav', 'tmp2.wav']
+            self.RunProcess(args)
+
+            audio_item.commands[i][2] = 'tmp2.wav'
+            self.RunProcess(audio_item.commands[i])
+
+        try:
+            pass
+            os.remove('tmp.wav')
+            os.remove('tmp2.wav')
+        except:
+            pass
+
+
+    def GenerateParams(self, event, in_list=None):
+        if in_list:
+            if len(in_list) == 1:
+                head, tail = os.path.split(in_list[0])
+                self.inputTextCtrl.SetValue(tail)
+            else:
+                commonpath = os.path.commonpath(in_list)
+                in_list_to_show = [name[len(commonpath)+1:] for name in in_list]
+                self.inputTextCtrl.SetValue("\n".join(in_list_to_show))
+            self.in_list = in_list
+
+        if not self.in_list:
+            return
+
+        outFormat = self.outFormatComboBox.GetClientData(self.outFormatComboBox.GetSelection())
+        speed_list = self.presetComboBox.GetClientData(self.presetComboBox.GetSelection())['speed']
+
+        head = self.outDirPicker.GetPath()
+        if not head or not os.path.exists(head):
+            head = os.path.abspath(os.path.expanduser('~'))
+            self.outDir = head
+            self.outDirPicker.SetPath(head)
+        self.outDir = head
+
+        self.audio_items = [AudioItem(item, speed_list, self.outDir, outFormat) for item in self.in_list]
+
+            
+        self.text.Clear()
+        for out_files in [audio_item.out_files for audio_item in self.audio_items]:
+            self.text.write(''.join([f"{out_file}\n" for out_file in out_files]))
+
+    def LockUi(self, lock=True):
+        if lock:
+            self.inputTextCtrl.Disable()
+            self.mergeCheck.Disable()
+            self.outDirPicker.Disable() 
+            self.progressGauge.Disable() 
+            self.presetComboBox.Disable() 
+            self.outFormatComboBox.Disable() 
+        else:
+            self.inputTextCtrl.Enable()
+            self.mergeCheck.Enable()
+            self.outDirPicker.Enable() 
+            self.progressGauge.Enable() 
+            self.presetComboBox.Enable() 
+            self.outFormatComboBox.Enable() 
+
+    def Convert(self, event):
+        if self.state == State.RUNNING:
+            self.state = State.INTERRUPTED
+            self.LockUi(False)
+            self.startButton.SetLabel("Start")
+            return
+        elif self.state == State.IDLE:
+            self.state = State.RUNNING
+            self.LockUi(True)
+            self.startButton.SetLabel("Stop")
+            self.text.Clear()
+
+
+        self.progressGauge.SetRange(len(self.audio_items) - 1)
+        self.progressGauge.SetValue(0)
+
+        self.processed_files = {} 
+        for i, audio_item in enumerate(self.audio_items):
+            if audio_item.input not in self.processed_files:
+                self.Speedup(None, audio_item)
+                self.processed_files[audio_item.input] = True
+            self.progressGauge.SetValue(i)
+
+            if self.state == State.INTERRUPTED:
+                self.progressGauge.SetValue(0)
+                self.state = State.IDLE
+                self.LockUi(False)
+                self.startButton.SetLabel("Start")
+                return
+
+        outFormat = self.outFormatComboBox.GetClientData(self.outFormatComboBox.GetSelection())
+        ext = outFormat['ext']
+
+        if self.mergeCheck.IsChecked():
+            with open('./merge.txt', 'w') as f:
+                for audio_item in self.audio_items:
+                    for out_file in audio_item.output:
+                        f.write(f"file '{out_file}'\n")
+            
+            merged_name = "_".join([str(speed) for speed in self.audio_items[0].speed_list]) + ext
+            merged_name = os.path.join(self.outDir, merged_name)
+            if ext == '.flac':
+                args = ['ffmpeg', '-y', '-f', 'concat', '-safe', '0', '-i', 'merge.txt', merged_name]
+            else:
+                args = ['ffmpeg', '-y', '-f', 'concat', '-safe', '0', '-i', 'merge.txt', '-c', 'copy', merged_name]
+            self.RunProcess(args)
+            os.remove('./merge.txt')
+
+            for audio_item in self.audio_items:
+                for out_file in audio_item.output:
+                    try:
+                        os.remove(out_file)
+                    except:
+                        pass
+
+        self.state = State.IDLE
+        self.LockUi(False)
+        self.startButton.SetLabel("Start")
+
+if __name__ == '__main__':
+    app = wx.App()
+    SpeechSpeedChangerGui()
+    app.MainLoop()
+
